0958860340639c50db19af83b040f162c45e6a57
mchardin <mchardin@student.42.fr>
2021-03-05 19:44:47 +0100

    auto tester

---
+++ check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+make
+cp main.cpp tests.cpp
+sed -i -e "s/ft::/std::/g" tests.cpp
+clang++ -Wall -Wextra -Werror -std=c++98 tests.cpp -o test_containers
+./ft_containers > my_res
+./test_containers > test_res
+diff my_res test_res
